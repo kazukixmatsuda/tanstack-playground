a4f91744ddd4c7385b1a2315a1ec09e82bb37282
kazukixmatsuda <xm.kazuki827@gmail.com>
2024-01-23 23:13:12 +0900

    install tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
+import { RouterProvider, Router } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
+const router = new Router({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
   test: {
     environment: "happy-dom",
     setupFiles: ["./vitest.setup.ts"],
